151f53fe0734ea82ee5228176bdf50dfbc0cd57b
Julian Müller <julian.mueller.ffb@kabelmail.de>
2016-11-04 23:46:31 +0100

    Added Player Movement
    Player is now aible to jump
    Added the Starting Point with a placeholder texture
    Added a placeholder texture for the player
    Set screen resolution when starting the game

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    // The force which is added when the player jumps
+    // This can be changed in the Inspector window
+    public Vector2 jumpForce = new Vector2(0, 300);
+    Rigidbody2D rb;
+
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Jump
+        if (Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector2.zero;
+            rb.AddForce(jumpForce);
+        }
+    }
+}
